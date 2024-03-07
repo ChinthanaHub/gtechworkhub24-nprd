21f46f7300ec4a2cb4f315ba695cf7a5580e09a7
Chinthana Silva <Chinthana_08172@dialog.lk>
2024-03-07 10:27:15 +0530

    Filesdeleted

--- modules/vpc/vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_db_subnet_group" "my_db_subnet_group" {
-  name = "my-db-subnet-group"
-  subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
-
-  tags = {
-    Name = "My DB Subnet Group"
-  }
-}
--- rds.tfvars
+++
@@ -1,3 +0,0 @@
-vpc_cidr = "10.20.0.0/16"
-vpc_name_tag = "terraform"
-vpc_project = "CG"
